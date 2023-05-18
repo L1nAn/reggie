316d2b86216ab8eae658d3d406f3f1c24aa4f836
L1nAn <821720533@qq.com>
2023-05-18 10:18:55 +0800

    Second submission

--- src/main/java/com/mzw/config/WebMvcConfig.java
+++ src/main/java/com/mzw/config/WebMvcConfig.java
@@ -5,8 +5,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
@@ -47,3 +49,17 @@ public class WebMvcConfig extends WebMvcConfigurationSupport {
         converters.add(0, messageConverter);//是0 的原因方式优先使用这个消息转化器
     }
 }
+//@EnableWebMvc
+//public class WebMvcConfig implements WebMvcConfigurer {
+//    @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        log.info("开始静态资源映射");
+//        registry.addResourceHandler("/backend/**").addResourceLocations("classpath:/backend/");
+//        registry.addResourceHandler("/front/**").addResourceLocations("classpath:/front/");
+//    }
+//
+//    @Override
+//    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+//        WebMvcConfigurer.super.extendMessageConverters(converters);
+//    }
+//}
